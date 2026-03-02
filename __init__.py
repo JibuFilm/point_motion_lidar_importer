43a7eff84854ba54d67eb1066f263fde8052d7de
Jibu <virgiljinjjb@gmail.com>
2026-03-02 14:07:12 -0800

    Update __init__.py
    license update

--- __init__.py
+++ __init__.py
@@ -7,6 +7,27 @@ Sequence loading logic inspired by blender-sequence-loader (MIT License)
 https://github.com/InteractiveComputerGraphics/blender-sequence-loader
 """
 
+# Point Motion LiDAR Importer
+# Copyright (C) 2026 JibuFilm
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# Part of an ongoing cinematic research project by JibuFilm
+# https://jibujin.com
+
+
+
 bl_info = {
     "name": "Point Motion LiDAR Importer",
     "author": "JibuFilm",
